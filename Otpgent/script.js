73f88a2e2a6ee540e908df3288bba5dc43418857
sachinkumarwbd <srpgkumaar1234@gmail.com>
2024-03-16 14:51:07 +0530

    changes

--- Otpgent/script.js
+++ Otpgent/script.js
@@ -1,20 +1,10 @@
 let button = document.querySelector("button");
 let input = document.querySelector("input");
 
-// button.addEventListener("click", (e) => {
-//   let opt = Math.floor(Math.random() * 9000 + 1000);
-//   console.log(opt);
-// });
-
 let generateOTP = () => {
-  //Define the length of the OTP
-
-  // Generate a random numeric OTP with exactly 6 digits
   const otp = Math.floor(100000 + Math.random() * 900000);
-  // console.log(otp);
-  document.querySelector("p").innerText = otp;
 
-  //Display the generated OTP
+  document.querySelector("p").innerText = otp;
 };
 
 document.querySelector("button").addEventListener("click", generateOTP);
